506c7360f6ee4b44f62324a7b1440d81b10e3fc9
Stephen Becker IV <github@deathbyescalator.com>
2017-01-06 14:31:16 -0800

    displays a map

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './App.css';
+import SimpleMapPage from './simple.js';
+
+class App extends Component {
+  render() {
+    return (
+	    <SimpleMapPage />
+    );
+  }
+}
+
+export default App;
---
+++ frontend/src/index.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css';
+import './main.css';
+import { withGoogleMap, GoogleMap,
+  Marker, } from "react-google-maps";
+import _ from "lodash";
+import {
+  Component,
+} from "react";
+
+
+const GettingStartedGoogleMap = withGoogleMap(props => (
+  <GoogleMap
+    ref={props.onMapLoad}
+    defaultZoom={3}
+    defaultCenter={{ lat: -25.363882, lng: 131.044922 }}
+    onClick={props.onMapClick}
+  >
+  </GoogleMap>
+));
+// Then, render it:
+
+ReactDOM.render(
+  <GettingStartedGoogleMap
+    containerElement={
+      <div style={{ height: `100%` }} />
+    }
+    mapElement={
+      <div style={{ height: `100%` }} />
+    }
+    onMapLoad={_.noop}
+    onMapClick={_.noop}
+    onMarkerRightClick={_.noop}
+  />,
+  document.getElementById('root')
+);
+
